64624ab16e0a2bf5926fdb90b49fbcee1edfdbda
ashishsahoo2001 <160223885+ashishsahoo2001@users.noreply.github.com>
2024-07-03 23:48:51 +0530

    few changes

--- ops/Demo.java
+++ ops/Demo.java
@@ -1,19 +1,59 @@
 package ops;
 
-public class Demo {
-    int a; String name;
-    Demo(int i,String n){
-a=i;
-name=n;
+/*class A{
+    private static int x;
+    int y;
+    void f1()
+    {
+        x=10;
+        System.out.println("x="+x);
     }
-    void show(){
-        System.out.println(a +" "+name);
+//    void f2()
+//    {
+//        System.out.println(x);
+//    }
+    void f3()
+    {
+        x=12;
+        System.out.println(x);
     }
 
+}
+class Demo{
     public static void main(String[] args) {
-        Demo r=new Demo(50,"ashish");
-        Demo f=new Demo(60, "lipu");
-        r.show();
-        f.show();
+        A a1=new A();
+
+        a1.f1();
+
+        a1.f3();
+
+    }
+}*/
+class A{
+    private int x;
+    private static int y;
+
+    void f1()
+    {
+        x=11;
+        y=22;
+    }
+        void f2()
+    {
+     System.out.println(x);
+    }
+   /* void f3()
+    {
+        x=12;
+        System.out.println(x);
+    }*/
+}
+class Demo
+{
+    public static void main(String[] args) {
+        A a1=new A();
+        A a2=new A();
+        a1.f1();
+        a2.f2();
     }
 }
